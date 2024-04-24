62b041937e708335add4cb535847e652eaddd0b9
sri charan <sricharankaparthi123@gmail.com>
2024-04-24 16:29:53 +0530

    installation-up[dated

--- installation-automation.sh
+++ installation-automation.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-echo "starting the installation"
-dnf install nginx -y
+echo "which software you want to execute "
+read SOFTWARE
+if [${id} -eq 0]
+then
+dnf install $SOFTWARE -y
+
 echo "completed the installation"
+else 
+echo "For installing any software you must have root access"
+fi
--- variables.sh
+++ variables.sh
@@ -9,10 +9,9 @@ echo "addition of ${Variable1} and ${Variable2} is ${Variable3}"
 Name=$1 #passing a input as an arguments from a user
 
 echo "name of the person is $Name"
-
 echo "Enter the candidate's name:"
 
-read -s USERNAME  # reads the input from the user and stores into variable (USERNAME)
+read  USERNAME  # reads the input from the user and stores into variable (USERNAME)
 # -s will restrict the visibility of the input on terminal
 echo "name of candidate is $USERNAME " 
 echo "num of arguments passed :$#" # $# will store the total no of arguements passed at the begining of the script execution
