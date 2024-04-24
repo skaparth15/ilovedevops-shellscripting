b8a275397cf0f26f0d10758854571af57ae531ba
sri charan <sricharankaparthi123@gmail.com>
2024-04-24 16:48:17 +0530

    some-message

--- installation-automation.sh
+++ installation-automation.sh
@@ -2,10 +2,9 @@
 echo "which software you want to execute "
 read SOFTWARE
 USER=$(id -u)
-if [${USER} -eq 0]
+if [ $USER -ne 0 ]
 then
 dnf install $SOFTWARE -y
-
 echo "completed the installation"
 else 
 echo "For installing any software you must have root access"
