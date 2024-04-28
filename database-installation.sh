3892f9854e2fd334fa90e179f603041a8d340902
sri charan <sricharankaparthi123@gmail.com>
2024-04-28 13:12:00 +0530

    new files

--- database-installation.sh
+++ database-installation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "started installing mys1l-server"
 userid=$(id -u)
-if [$userid -eq 0]
+if [ ${userid} -eq 0 ]
 then 
     echo "you are a root user, have access to install softwares"
     dnf install mysql-server -y
