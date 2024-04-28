695b6d82ff3fa20a925043f33a1d07620cae050c
sri charan <sricharankaparthi123@gmail.com>
2024-04-28 13:18:05 +0530

    new files

--- database-installation.sh
+++ database-installation.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 echo "started installing mys1l-server"
 userid=$(id -u)
-if [ ${userid} -eq 0 ]
-then 
-    echo "you are a root user, have access to install softwares"
-    dnf install mysql-server -y
-    echo "sucessfully installed mysql-server"
-else 
-     
-     echo "For Installing you should have root previliges"
 
-fi
+userVerification(){
+    if [ ${userid} -eq 0 ]
+    then 
+        echo "you are a root user, have access to install softwares"
+    else 
+         echo "For Installing you should have root previliges"
+         exit 1
+    fi
+
+}
+    $userVerification
+    dnf install mysql-server -y
 
  
